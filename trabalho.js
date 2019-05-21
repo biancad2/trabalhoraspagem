4996b8430d6247cff68bb8df4e5d46c2afff8fa2
Bianca Duarte <bianca.duarte.barreto@hotmail.com>
2019-05-21 13:22:19 +0000

    primeiro commit

---
+++ trabalho.js
@@ -0,0 +1,38 @@
+//AI IMOVEIS
+function montarPagina(pagina){
+    var parser = new DOMParser();
+    var documento = parser.parseFromString(pagina, "text/html");
+    documento.querySelectorAll(".search-default").forEach(function(div){
+        var as = div.children;
+        Array.from(as).forEach(function(a){
+            var noticia = a.children;
+            Array.from(noticia).forEach(x => document.body.appendChild(x));
+        })
+    });
+}
+
+
+function sendReq() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        montarPagina(this.responseText);
+        //alert("OI");
+      }
+    };
+  xhttp.open("GET", "https://www.aiimoveis.com/", true);
+  xhttp.send();
+}
+
+function teste(){
+    sendReq();
+}
+
+
+function criarEvento(){
+    document.querySelector("button").addEventListener("click",function(){
+        teste();
+    })
+}
+
+window.onload = criarEvento;
